9f4bbf48da552af0b37c051ce19edd5d6c390197
datbn2002 <145431569+datbn2002@users.noreply.github.com>
2023-09-26 13:18:02 +0700

    dat de_1.py

--- de_1.py
+++ de_1.py
@@ -1,12 +1,14 @@
-# de 1 mon ngon ngu lap trinh Python
-class tugiac():
-    def __init__(self,a,b,c,d):
+# Định nghĩa lớp tugiac
+class tugiac:
+    def __init__(self, a, b, c, d):
         self.a = a
         self.b = b
         self.c = c
         self.d = d
+
     def chuvi(self):
-        return self.a+self.b+self.c+self.d
+        return self.a + self.b + self.c + self.d
+
     def canhlon(self):
         m = self.a
         if m < self.b:
@@ -16,52 +18,59 @@ class tugiac():
         if m < self.d:
             m = self.d
         return m
-'''obj = tugiac(2,4,4,6)
-print('chu vi = ',obj.chuvi())
-print('canh lon nhat:',obj.canhlon())'''
+
+from tkinter import *
+
+ds = []
+
+# Hàm để nhận dữ liệu từ người dùng
 def nhapdulieu():
     c1 = canh1.get()
     c2 = canh2.get()
     c3 = canh3.get()
     c4 = canh4.get()
-    obj = tugiac(c1,c2,c3,c4)
+    obj = tugiac(c1, c2, c3, c4)
     ds.append(obj)
+
+# Hàm để tính toán và hiển thị kết quả
 def tinhtoan():
     s = []
     cv = []
-    for i in range(0,len(ds)):
-        stam =  'chu vi hinh ' +str(i) + ':' + str(ds[i].chuvi()) + 'do dai canh lon = ' + str(ds[i].canhlon())
+    for i in range(0, len(ds)):
+        stam = 'chu vi hình ' + str(i) + ': ' + str(ds[i].chuvi()) + ', độ dài cạnh lớn = ' + str(ds[i].canhlon())
         s.append(stam)
         cv.append(ds[i].chuvi())
-    label2.config(text = s[0])
-    label2.pack()
-    label3.config(text = s[1])
-    label3.pack()
-    label4.config(text = s[2])
-    label4.pack()
-    s2 = 'Chu vi nho nhat ' + str(min(cv))
-    label5.config(text = s2)
-    label5.pack()
-from tkinter import *
-ds = []
+    
+    # Hiển thị kết quả trên các Label
+    label2.config(text=s[0])
+    label3.config(text=s[1])
+    label4.config(text=s[2])
+    s2 = 'Chu vi nhỏ nhất: ' + str(min(cv))
+    label5.config(text=s2)
+
 w = Tk()
 w.geometry('300x400')
-w.title('Bai tap so 1')
-label1 = Label(w,text ='Hay nhap 4 canh :').pack()
-canh1= DoubleVar()
-canh2= DoubleVar()
-canh3= DoubleVar()
-canh4= DoubleVar()
-E1 = Entry(w,textvariable = canh1).pack()
-E2 = Entry(w,textvariable = canh2).pack()
-E3 = Entry(w,textvariable = canh3).pack()
-E4 = Entry(w,textvariable = canh4).pack()
-b1 = Button(w,text = 'Them du lieu',command = nhapdulieu).pack()
-b2 = Button(w,text = 'Tinh toan', command = tinhtoan).pack()
-label2  = Label(w)
+w.title('Bài tập số 1')
+
+# Label và Entry để nhập dữ liệu
+Label(w, text='Hãy nhập 4 cạnh:').pack()
+canh1 = DoubleVar()
+canh2 = DoubleVar()
+canh3 = DoubleVar()
+canh4 = DoubleVar()
+Entry(w, textvariable=canh1).pack()
+Entry(w, textvariable=canh2).pack()
+Entry(w, textvariable=canh3).pack()
+Entry(w, textvariable=canh4).pack()
+
+# Button để thêm dữ liệu và tính toán
+Button(w, text='Thêm dữ liệu', command=nhapdulieu).pack()
+Button(w, text='Tính toán', command=tinhtoan).pack()
+
+# Label để hiển thị kết quả
+label2 = Label(w)
 label3 = Label(w)
 label4 = Label(w)
 label5 = Label(w)
-                  
 
-
+w.mainloop()
