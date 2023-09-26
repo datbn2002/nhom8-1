f06a44e11643f051a9f7f14a39d575e328c7c5b0
datbn2002 <145431569+datbn2002@users.noreply.github.com>
2023-09-26 14:47:45 +0700

    Update thong_ke.py

--- thong_ke.py
+++ thong_ke.py
@@ -1,23 +1,31 @@
 import pandas as pd
-from numpy import array
 import matplotlib.pyplot as plt
-import numpy as np
-df=pd.read_csv('diemPython.csv',index_col=0,header = 0)
-in_data = array(df.iloc[:,:])
-print(in_data)
-print('Tong so sinh vien di thi :')
-tongsv= in_data[:,1]
-print(np.sum(tongsv))
-diemA = in_data[:,3]
-diemBc = in_data[:,4]
-print('Tong sv:',tongsv)
-maxa = diemA.max()
-i, = np.where(diemA == maxa)
-print('lop co nhieu diem A la {0} co {1} sv dat diem A'.format(in_data[i,0],maxa))
-plt.plot(range(len(diemA)),diemA,'r-',label="Diem A")
-plt.plot(range(len(diemBc)),diemBc,'g-',label="Diem B +")
-plt.xlabel('Lơp')
-plt.ylabel(' So sv dat diem ')
+
+# Đọc dữ liệu từ tệp CSV và đặt cột đầu tiên là cột chỉ mục
+df = pd.read_csv('diemPython.csv', index_col=0, header=0)
+
+# Lấy tất cả dữ liệu từ DataFrame thành một mảng NumPy
+data = df.values
+
+# In dữ liệu
+print(data)
+
+# Tính tổng số sinh viên tham gia thi
+tong_sinh_vien = data[:, 1].sum()
+print(f'Tổng số sinh viên dự thi: {tong_sinh_vien}')
+
+# Tính điểm cao nhất của lớp và số sinh viên đạt điểm A
+diem_A = data[:, 3]
+diem_Bc = data[:, 4]
+max_diem_A = diem_A.max()
+lop_max_diem_A = df.index[diem_A.argmax()]
+so_sv_dat_diem_A = (diem_A == max_diem_A).sum()
+print(f'Lớp có nhiều điểm A nhất là {lop_max_diem_A} với {so_sv_dat_diem_A} sinh viên đạt điểm A.')
+
+# Vẽ biểu đồ
+plt.plot(range(len(diem_A)), diem_A, 'r-', label="Diem A")
+plt.plot(range(len(diem_Bc)), diem_Bc, 'g-', label="Diem B +")
+plt.xlabel('Lớp')
+plt.ylabel('Số sinh viên đạt điểm')
 plt.legend(loc='upper right')
 plt.show()
-      
